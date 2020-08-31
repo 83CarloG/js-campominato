157911236dab0777da75182b382dcedc2010ca6b
Carlo Giuseppe Colombo <cgcolombo1983@hotmail.com>
2020-08-31 16:43:56 +0200

    nuovo

--- js/script.js
+++ js/script.js
@@ -6,31 +6,55 @@
 //	La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
 //	Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
 
+// BONUS: (da fare solo se funziona tutto il resto)
+// all’inizio il software richiede anche una difficoltà all’utente che cambia il range di numeri casuali:
+// con difficoltà 0 => tra 1 e 100
+// con difficoltà 1 =>  tra 1 e 80
+// con difficoltà 2 => tra 1 e 50
+
 //	funzione che genera un numero casuale tra 1 e 100
 function numberRandom (min, max) {
 	var numRandom = Math.floor(Math.random() * (max - min + 1)) + min;
 	return numRandom;
 }
 //	funzione che dato un array e un numero mi controlla che il numero non sia già nell'array
-function checkDoppione (arr, numero) {
+function inArray (arr, elemento) {
 	for (var i = 0; i < arr.length; i++) {
-		if (arr[i] === numero) {
+		if (arr[i] === elemento) {
 			return true;
 		}
 	}
 	return false;
 }
-// Numero Bome - MODIFICABILE
+// Numero Bombe - MODIFICABILE
 var bombe = 16;
 //	creo variabile array che contiene i numeri bomba
 var arrBomba = [];
 // Array tentativi
 var arrTentativi = [];
+// imposto il livello e i tentativi
+alert('Benvenuto!! Stai per entrare nel Campo Minato! \n Scegli un livello di difficoltà \n');
+var maxTentativiBombe;
+var livello = parseInt(prompt('Inserisci la difficolta(0/1/2)'));
+switch (livello) {
+	case 0:
+		maxTentativiBombe = 100;
+		break;
+	case 1:
+		maxTentativiBombe = 80;
+		break;
+	case 2:
+		maxTentativiBombe = 50;
+		break;
+	default:
+		maxTentativiBombe = 100;
+}
+alert('Pronto? Inserisci un numero da 1 a ' + maxTentativiBombe + ' e spera di non pestare una Bomba')
 // Riempo l'arrBomba con le mie bombe univoche
 for (var i = 0; i < bombe; i++) {
-	var numeroBombaProvv = numberRandom(1, 100);
-	while (checkDoppione(arrBomba, numeroBombaProvv)) {
-		numeroBombaProvv = numberRandom(1, 100);
+	var numeroBombaProvv = numberRandom(1, maxTentativiBombe);
+	while (inArray(arrBomba, numeroBombaProvv)) {
+		numeroBombaProvv = numberRandom(1, maxTentativiBombe);
 	}
 	arrBomba.push(numeroBombaProvv);
 }
@@ -38,49 +62,29 @@ for (var i = 0; i < bombe; i++) {
 console.log(arrBomba);
 // variabili booleana haPerso = false
 var haiPerso = false;
-// imposto il livello e i tentativi
-alert('Benvenuto!! Stai per entrare nel Campo Minato! \n 1) Scegli un livello di difficoltà \n 2) Inserisci un numero da 1 a 100 e spera di non pestare una Bomba' )
-var maxTentativiBombe = parseInt(prompt('Inserisci la difficolta(0/1/2)'));
-while (!((maxTentativiBombe - 0) * (maxTentativiBombe - 2) <= 0)) {
-	maxTentativiBombe = parseInt(prompt('Inserisci la difficolta(0/1/2)'));
-}
-if (maxTentativiBombe === 0) {
-	maxTentativiBombe = 100 - bombe;
-}	else if (maxTentativiBombe === 1) {
-	maxTentativiBombe = 80 - bombe;
-} else if (maxTentativiBombe === 2) {
-	maxTentativiBombe = 50 - bombe;
-};
 // Inizio il ciclo do-while per verificare i tentativi del giocatore e dare un esito
 var numeroTentativi = 0;
 do {
 	// prompt in cui chiedo all'utente un numero
-	var tentativo = parseInt(prompt('inserisci un numero da 1 a 100'));
+	var tentativo = parseInt(prompt('inserisci un numero da 1 a ' + maxTentativiBombe));
 	// check che il numero sia una variabile di tipo number
-	while (isNaN(tentativo)) {
-		tentativo = parseInt(prompt('Non hai inserito un numero da 1 a 100'));
-	}
 	// check che il numero sia compreso tra 1 e 100
-	while (tentativo < 1 || tentativo > 100) {
-		tentativo = parseInt(prompt('Non hai inserito un numero da 1 a 100'));
+	while (isNaN(tentativo) || tentativo < 1 || tentativo > maxTentativiBombe ) {
+		tentativo = parseInt(prompt('Non hai inserito un numero da 1 a ' + maxTentativiBombe));
 	}
 	// check che il numero non sia duplicato nell'array utente
-	while (checkDoppione(arrTentativi, tentativo)) {
-		tentativo = parseInt(prompt('Numero già inserito!! inserisci un numero da 1 a 100'));
+	while (inArray(arrTentativi, tentativo)) {
+		tentativo = parseInt(prompt('Numero già inserito!! inserisci un numero da 1 a ' + maxTentativiBombe));
 	}
 	tentativo = parseInt(tentativo);
 	// se non è duplicato, salvo il numero nell'array utente
 	arrTentativi.push(tentativo);
 	// check che il numero non sia nell'array bomba
-	haiPerso = checkDoppione(arrBomba, tentativo);
+	haiPerso = inArray(arrBomba, tentativo);
 	numeroTentativi++;
 	console.log(tentativo);
 	console.log(arrTentativi);
 } while ((numeroTentativi < maxTentativiBombe) && (!haiPerso));
 
 // Comunico il risultato al giocatore
-if (haiPerso) {
-	alert('BOOOOOOOOOOM Hai perso, i tuoi tentativi sono stati ' + arrTentativi.length + '!');
-} else {
-	alert('HAI VINTO!! i tuoi tentativi sono stati ' + arrTentativi.length + '!');
-}
+haiPerso ? alert('BOOOOOOOOOOM Hai perso, i tuoi tentativi sono stati ' + arrTentativi.length + '!') : alert('HAI VINTO!! i tuoi tentativi sono stati ' + arrTentativi.length + '!');
